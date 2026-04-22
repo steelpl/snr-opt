21c7c7f8d5188d7ae74d266c0420983f753bfe3e
Seokhyeon Kim <seokhyn.kim@gmail.com>
2026-04-22 16:50:54 +0900

    Revise reference citation in EeeTGEN.m
    Updated reference citation to the latest publication year and details.

--- MATLAB scripts/EeeTGEN.m
+++ MATLAB scripts/EeeTGEN.m
@@ -27,9 +27,9 @@ function EeeT = EeeTGEN(p,ecc,var_e)
 % REFERENCE
 % For more details, see:
 %
-% Kim, S., Sharma, A., Liu, Y. Y., & Young, S. I. (2021). 
-% Rethinking Satellite Data Merging: From Averaging to SNR Optimization.
-% IEEE Trans Geosci Remote Sens
+%   Kim, S., Sharma, A., Liu, Y. Y., & Young, S. I. (2022).
+%   Rethinking Satellite Data Merging: From Averaging to SNR Optimization.
+%   IEEE Trans Geosci Remote Sens., 60, 1–15.
 %
 % If you use the methods presented in the paper and/or this example, 
 % please cite this paper where appropriate.
