7a035310097c521c29406372c08c5ad8fb24b5f7
Seokhyeon Kim <seokhyn.kim@gmail.com>
2026-04-22 16:51:48 +0900

    Update reference year in WA.m documentation

--- MATLAB scripts/WA.m
+++ MATLAB scripts/WA.m
@@ -32,9 +32,9 @@ function u = WA(EeeT)
 %     when the input datasets are on a common scale.
 %
 % REFERENCE
-%   Kim, S., Sharma, A., Liu, Y. Y., & Young, S. I. (2021).
+%   Kim, S., Sharma, A., Liu, Y. Y., & Young, S. I. (2022).
 %   Rethinking Satellite Data Merging: From Averaging to SNR Optimization.
-%   IEEE Trans Geosci Remote Sens.
+%   IEEE Trans Geosci Remote Sens., 60, 1–15.
 %
 %   If you use this method, please cite the above reference.
 % ============================================================
